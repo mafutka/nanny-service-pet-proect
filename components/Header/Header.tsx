37bf18543883e1e8acac79a755b0f94dc094ce96
mafutka <mafutka@gmail.com>
2026-01-17 21:25:07 +0200

    login-register-added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,30 @@
+'use client'
+
 import Navigation from "../Navigation/Navigation"
+import { useState } from "react";
+import AuthModal from "../AuthModal/AuthModal";
 import css from "./Header.module.css"
 
 export default function Header() {
+    const [authType, setAuthType] = useState<"login" | "register" | null>(null);
+    const isLoggedIn = false;
     return (
         <header className={css.header}>
             <p className={css.headerLogo}>Nanny.Services</p>
             <Navigation />
+            {!isLoggedIn && (
+  <>
+    <button onClick={() => setAuthType("login")}>Log in</button>
+    <button onClick={() => setAuthType("register")}>Register</button>
+  </>
+)}
+
+{authType && (
+  <AuthModal
+    type={authType}
+    onClose={() => setAuthType(null)}
+  />
+)}
         </header>
     )
 
--- components/HomeHero/HomeHero.tsx
+++ components/HomeHero/HomeHero.tsx
@@ -1,30 +1,45 @@
 import css from "./HomeHero.module.css"
 
-export default function HomeHero({children,}: {children: React.ReactNode}) {
-    return (
-        <section className={css.hero}>
-            {children}
-        <div className={css.HeroContainer}>
+export default function HomeHero({ children }: { children: React.ReactNode }) {
+  return (
+    <section className={css.hero}>
+      {children}
+      <div className={css.HeroContainer}>
         <div className={css.heroInfo}>
-            <h1 className={css.heroTitle}>Make Life Easier for the Family:</h1>
-            <p className={css.heroText}>Find Babysitters Online for All Occasions</p>
-            <button className={css.heroBtn}>Get started 
-                <span>
-
-                </span>
-            </button>
+          <h1 className={css.heroTitle}>Make Life Easier for the Family:</h1>
+          <p className={css.heroText}>
+            Find Babysitters Online for All Occasions
+          </p>
+          <button className={css.heroBtn}>
+            Get started
+            <span>
+              <svg width="14" height="16" className={css.heroIcon1}>
+                <use href="/symbol-defs.svg#icon-Arrow-16"></use>
+              </svg>
+            </span>
+          </button>
         </div>
+      </div>
+      <div className={css.imgWrapper}>
+        <img
+          className={css.heroImg}
+          src="/HeroPicture.jpg"
+          width="699"
+          height="736"
+          alt="Hero Picture"
+        />
+        <div className={css.badgeWrapper}>
+            <div className={css.bedjeIcon}>
+            <svg width="28" height="18" className={css.heroIcon2}>
+                <use href="/symbol-defs.svg#icon-feCheck0"></use>
+              </svg>
+              </div>
+          <div className={css.badjeInfo}>
+            <p className={css.badjeText}>Experienced nannies</p>
+            <h3 className={css.badjeNumber}>15,000</h3>
+          </div>
         </div>
-        <div className={css.imgWrapper}>
-            <img className={css.heroImg} src="/HeroPicture.jpg" width="699" height="736" alt="Hero Picture" />
-            <div className={css.badgeWrapper}>
-            <div className={css.badjeInfo}>
-                <p className={css.badjeText}>Experienced nannies</p>
-                <h3 className={css.badjeNumber}>15,000</h3>
-            </div>
-        </div>
-        </div>
-    
-        </section>
-    )
+      </div>
+    </section>
+  )
 }
